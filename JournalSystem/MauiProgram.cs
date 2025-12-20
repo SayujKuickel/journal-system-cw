18b2a1546addb43a8c43afe0999a456bcff1b9c2
sayujkuickel <sayujkuickel@gmail.com>
2025-12-20 16:55:19 +0545

    !feat: added journal adding/deleting feature with basic search

--- JournalSystem/MauiProgram.cs
+++ JournalSystem/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 
 namespace JournalSystem
 {
@@ -15,9 +16,9 @@ namespace JournalSystem
                 });
 
             builder.Services.AddMauiBlazorWebView();
-
+            builder.Services.AddMudServices();
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Services.AddBlazorWebViewDeveloperTools();
     		builder.Logging.AddDebug();
 #endif
 
---
+++ JournalSystem/Models/JournalEntry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JournalSystem.Models
+{
+    public enum MoodCategory
+    {
+        Positive,
+        Neutral,
+        Negative
+    }
+
+    public class JournalEntry
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime EntryDate { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public MoodCategory PrimaryMood { get; set; }
+        public List<string> SecondaryMoods { get; set; } = new();
+        public List<string> Tags { get; set; } = new();
+        public string? Category { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+}
