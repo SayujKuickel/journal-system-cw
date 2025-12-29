51671442a4bab4c9f1663bb611d1676f3a25c522
sayujkuickel <sayujkuickel@gmail.com>
2025-12-29 10:21:22 +0545

    !fix: added platform folder

---
+++ JournalSystem/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace JournalSystem
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
